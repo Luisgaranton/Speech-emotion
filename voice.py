1f6bf4472f100f2ee684dee2575f0e7ccfdc0a76
Luis <luisgaranton04@gmail.com>
2023-03-12 22:35:28 -0300

    Arriba

--- voice.py
+++ voice.py
@@ -10,24 +10,25 @@ if not sys.warnoptions:
     warnings.simplefilter("ignore")
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 
-speech_auido = "C:\\Users\\luisg\\speech-emotion\\dataset\\"
+speech_audio = "C:\\Users\\luisg\\speech-emotion\\dataset\\"
 
-speech_auido_directory_list = os.listdir(speech_auido)
+speech_audio_directory_list = os.listdir(speech_audio)
 file_emotion = []
 file_statement = []
 file_path = []
-for dir in speech_auido_directory_list:
+for dir in speech_audio_directory_list:
     # as their are 20 different actors in our previous directory we need to extract files for each actor.
-    actor = os.listdir(speech_auido + dir)
+    actor = os.listdir(speech_audio + dir)
     for file in actor:
         part = file.split('.')[0]
         part = part.split('-')
         # third part in each file represents the emotion associated to that auido.
         file_emotion.append(int(part[2]))
         file_statement.append(int(part[4]))
-        file_path.append(speech_auido + dir + '/' + file)
+        file_path.append(speech_audio + dir + '/' + file)
 
-        speech_auido_df = pd.DataFrame({"Emotions":file_emotion, "Statement":file_statement, "Path":file_path})
+        speech_audio_df = pd.DataFrame({"Emotions":file_emotion, "Statement":file_statement, "Path":file_path})
 
-        speech_auido_df.Emotions.replace({1:'neutral', 2:'calm', 3:'happy', 4:'sad', 5:'angry', 6:'fear', 7:'disgust', 8:'surprise'}, inplace=True)
-speech_auido_df.head()
+        speech_audio_df.Emotions.replace({1:'neutral', 2:'calm', 3:'happy', 4:'sad', 5:'angry', 6:'fear', 7:'disgust', 8:'surprise'}, inplace=True)
+speech_audio_df.head()
+speech_auido_df.to_csv('speech-audio.csv')
